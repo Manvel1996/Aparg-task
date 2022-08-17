73b2b13b5dc8ee21eaaa7d3c2134f06f0daddf36
Manvel1996 <manvo00@mail.ru>
2022-08-18 00:07:17 +0400

    add key

--- src/ImageTask/ImageTaskMain/BasketPhotos/BasketPhotos.js
+++ src/ImageTask/ImageTaskMain/BasketPhotos/BasketPhotos.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './BasketPhotos.scss'
+import { v4 as uuidv4 } from 'uuid';
 
 export default function BasketPhotos({basketArrPhotos}) {
   return (
     <div className='basketPhotos'>
         {basketArrPhotos.map(el=>{
             return(
-                <img src={el}/>
+                <img src={el} key={uuidv4()}/>
             )
         })}
     </div>
